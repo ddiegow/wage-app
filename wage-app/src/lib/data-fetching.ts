672e887202da31ffabe454fbb4c058a80764bde6
Daniel Diego Wildin <ddwildin@gmail.com>
2025-02-17 11:33:41 +0900

    start wage project

---
+++ wage-app/src/lib/data-fetching.ts
@@ -0,0 +1,50 @@
+import { translatedJobs, translatedPrefectures } from "./data-translation";
+import { VALUE } from "./types";
+
+export const fetchData = async () => {
+    try {
+        const response = await fetch('/data/wage_data.json');
+        if (!response.ok)
+            throw new Error('Could not fetch local data')
+        return await response.json();
+    } catch (error) {
+        console.error('Error while fetching data: ', error);
+        return null;
+    }
+};
+
+export const getJobs = (): { code: string, name: string }[] => {
+    return translatedJobs;
+}
+
+export const getPrefectures = (): { code: string, name: string }[] => {
+    return translatedPrefectures;
+}
+
+export const getByJobAndPrefecture = (values: VALUE[], prefectureCode: string, jobCode: string): number | null => {
+    const entry = values.find(v => v["@cat02"] == jobCode && v["@area"] == prefectureCode);
+    if (!entry)
+        return null;
+    return Number(entry.$);
+}
+export const getByPrefecture = (values: VALUE[], prefectureCode: string): { job: { code: string, name: string }, amount: number }[] => {
+    const jobs = getJobs();
+    const entries: { job: { code: string, name: string }, amount: number }[] = []
+    for (const job of jobs) {
+        const amount = getByJobAndPrefecture(values, prefectureCode, job.code)
+        if (amount)
+            entries.push({ job: job, amount: amount })
+    }
+    return entries
+}
+
+export const getByJob = (values: VALUE[], jobCode: string): { prefecture: { code: string, name: string }, amount: number }[] => {
+    const prefectures = getPrefectures();
+    const entries: { prefecture: { code: string, name: string }, amount: number }[] = []
+    for (const prefecture of prefectures) {
+        const amount = getByJobAndPrefecture(values, prefecture.code, jobCode)
+        if (amount)
+            entries.push({ prefecture: prefecture, amount: amount })
+    }
+    return entries
+}
---
+++ wage-app/src/lib/types.ts
@@ -0,0 +1,115 @@
+export interface WageData {
+    GET_STATS_DATA: GETSTATSDATA;
+}
+
+export interface GETSTATSDATA {
+    RESULT: RESULT;
+    PARAMETER: PARAMETER;
+    STATISTICAL_DATA: STATISTICALDATA;
+}
+
+export interface STATISTICALDATA {
+    RESULT_INF: RESULTINF;
+    TABLE_INF: TABLEINF;
+    CLASS_INF: CLASSINF;
+    DATA_INF: DATAINF;
+
+}
+
+export interface DATAINF {
+    NOTE: NOTE[];
+    VALUE: VALUE[];
+}
+
+export interface VALUE {
+    '@tab': string;
+    '@cat01': string;
+    '@cat02': string;
+    '@area': string;
+    '@time': string;
+    '@unit': string;
+    '$': string;
+}
+
+export interface NOTE {
+    '@char': string;
+    '$': string;
+}
+
+export interface CLASSINF {
+    CLASS_OBJ: CLASSOBJ[];
+}
+
+export interface CLASSOBJ {
+    '@id': string;
+    '@name': string;
+    CLASS: CLASS[];
+}
+
+export interface CLASS {
+    '@code': string;
+    '@name': string;
+    '@level': string;
+    '@unit'?: string;
+    '@parentCode'?: string;
+}
+
+export interface TABLEINF {
+    '@id': string;
+    STAT_NAME: STATNAME;
+    GOV_ORG: STATNAME;
+    STATISTICS_NAME: string;
+    TITLE: string;
+    CYCLE: string;
+    SURVEY_DATE: number;
+    OPEN_DATE: string;
+    SMALL_AREA: number;
+    COLLECT_AREA: string;
+    MAIN_CATEGORY: STATNAME;
+    SUB_CATEGORY: STATNAME;
+    OVERALL_TOTAL_NUMBER: number;
+    UPDATED_DATE: string;
+    STATISTICS_NAME_SPEC: STATISTICSNAMESPEC;
+    DESCRIPTION: string;
+    TITLE_SPEC: TITLESPEC;
+}
+
+export interface TITLESPEC {
+    TABLE_CATEGORY: string;
+    TABLE_NAME: string;
+}
+
+export interface STATISTICSNAMESPEC {
+    TABULATION_CATEGORY: string;
+}
+
+export interface STATNAME {
+    '@code': string;
+    '$': string;
+}
+
+export interface RESULTINF {
+    TOTAL_NUMBER: number;
+    FROM_NUMBER: number;
+    TO_NUMBER: number;
+    NEXT_KEY: number;
+}
+
+export interface PARAMETER {
+    LANG: string;
+    STATS_DATA_ID: string;
+    DATA_FORMAT: string;
+    START_POSITION: number;
+    METAGET_FLG: string;
+    EXPLANATION_GET_FLG: string;
+    ANNOTATION_GET_FLG: string;
+    REPLACE_SP_CHARS: number;
+    CNT_GET_FLG: string;
+    SECTION_HEADER_FLG: number;
+}
+
+export interface RESULT {
+    STATUS: number;
+    ERROR_MSG: string;
+    DATE: string;
+}
