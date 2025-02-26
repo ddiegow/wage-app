bf2c886549c3743b93b0986224f484fa00512938
Daniel Diego Wildin <ddiegow@uoc.edu>
2025-02-26 14:33:47 +0900

    correct coloring to use downward slope

--- wage-app/src/lib/coloring.ts
+++ wage-app/src/lib/coloring.ts
@@ -1,1 +1,4 @@
-export const ValueToColor = (min: number, max: number, value: number) => 270 * (value - min) / (max - min)
+// calculate the donward slope of the mapping of values from the minimum to the maximum
+// when it's mapped to 0-270 to use in hsl (minimum -> 270, maximum -> 0)
+
+export const ValueToColor = (min: number, max: number, value: number) => (270 / (min - max)) * (value - max)
