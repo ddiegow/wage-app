41ecab188a0b2b12f17ecdd9492f8f179619cd2d
Daniel Diego Wildin <ddiegow@uoc.edu>
2025-03-01 09:56:31 +0900

    clean up map component generation code

--- wage-app/src/lib/map-component-generation.tsx
+++ wage-app/src/lib/map-component-generation.tsx
@@ -36,95 +36,119 @@ export const GenerateMapElements = (
         if (prefectureTooltip)
             tooltip = tooltip + prefectureTooltip.prefecture.name + (prefectureTooltip.tooltip ? " - " + prefectureTooltip.tooltip : "")
         // if we're at a polygon element
-        if (g.querySelectorAll<SVGPolygonElement>("polygon").length) {
-            // create a new react element
-            const polygons = Array.from(g.querySelectorAll<SVGPolygonElement>("polygon"));
-            const pathDefs: string[] = [];
-
-            // convert polygons to paths
-            polygons.forEach((polygon) => {
-                const points = polygon.getAttribute("points");
-                if (!points) return;
-
-                // parse points and create path data
-                const coordinates = points.trim().split(/\s+/).map(Number);
-                if (coordinates.length < 6 || coordinates.length % 2 !== 0) return;
-
-                let pathData = `m ${coordinates[0]} ${coordinates[1]}`;
-                for (let i = 2; i < coordinates.length; i += 2) {
-                    const dx = coordinates[i] - coordinates[i - 2];
-                    const dy = coordinates[i + 1] - coordinates[i - 1];
-                    pathData += ` ${dx} ${dy}`;
-                }
-                pathData += "z";
-                pathDefs.push(pathData);
-            });
-
-            // create new path elements
-            const paths = pathDefs.map(d =>
-                <path key={uuidv4()} d={d} />
-            );
-
-            return (
-                <g
-                    key={uuidv4()}
-                    transform={g.getAttribute("transform") || ""}
-                    className={g.classList.toString() + " hover:cursor-pointer"}
-                    strokeLinejoin="round"
-                    fill={color}
-                    fillRule="nonzero"
-                    stroke="#000000"
-                    strokeWidth="1.0"
-                    data-code={g.getAttribute("data-code") || ""}
-                    onMouseOver={() => setHoveredPrefecture(prefCode)}
-                    onMouseLeave={() => setHoveredPrefecture("")}
-                    onClick={(e) => handleClick(e.currentTarget.getAttribute("data-code") || "")}
-                >
-                    <title>{tooltip}</title>
-                    {paths}
-                </g >
-            );
-        } else {
-            // no polygons, create a new react element
-            // and keep the original children
-            const children = Array.from(g.childNodes).map((child) => {
-                if (child.nodeType === Node.ELEMENT_NODE) {
-                    // create a jsx element for each child element
-                    const element = child as Element;
-                    // if it's a title element
-                    if (element.tagName === 'title') {
-                        // create a jsx title element
-                        return <title key={uuidv4()}>{tooltip}</title>;
-                        // if it's a path element
-                    } else if (element.tagName === 'path') {
-                        // create a jsx path element
-                        const pathElement = element as SVGPathElement;
-                        return <path
-                            key={uuidv4()}
-                            d={pathElement.getAttribute('d') || ""}
-                        />;
-                    }
-                }
-                // if it's another kind (which there isn't) return null
-                return null;
-            });
-            return (
-                <g
-                    key={uuidv4()}
-                    transform={g.getAttribute("transform") || ""}
-                    className={g.classList.toString() + " hover:cursor-pointer"}
-                    fill={color}
-                    stroke={g.getAttribute("stroke") || "#000000"}
-                    strokeWidth={g.getAttribute("stroke-width") || "1.0"}
-                    onMouseOver={() => setHoveredPrefecture(prefCode)}
-                    onMouseLeave={() => setHoveredPrefecture("")}
-                    data-code={g.getAttribute("data-code") || ""}
-                    onClick={(e) => handleClick(e.currentTarget.getAttribute("data-code") || "")}
-                >
-                    {children}
-                </g>
-            );
-        }
+        return (g.querySelectorAll<SVGPolygonElement>("polygon").length) ?
+            // generate an svg element with paths from a polygon element
+            generatePathsFromPolygon(
+                g,
+                color,
+                tooltip,
+                () => setHoveredPrefecture(prefCode),
+                () => setHoveredPrefecture(""),
+                (e) => handleClick(e.currentTarget.getAttribute("data-code") || "")
+            )
+            :
+            generateReactG(
+                g,
+                color,
+                tooltip,
+                () => setHoveredPrefecture(prefCode),
+                () => setHoveredPrefecture(""),
+                (e) => handleClick(e.currentTarget.getAttribute("data-code") || "")
+            )
     });
     return mapping;
+}
+
+const generatePathsFromPolygon = (g: SVGElement, color: string, tooltip: string, onMouseOver: () => void, onMouseLeave: () => void, onClick: (e: React.MouseEvent<SVGGElement, MouseEvent>) => void
+) => {
+    // create a new react element
+    const polygons = Array.from(g.querySelectorAll<SVGPolygonElement>("polygon"));
+    const pathDefs: string[] = [];
+
+    // convert polygons to paths
+    polygons.forEach((polygon) => {
+        const points = polygon.getAttribute("points");
+        if (!points) return;
+
+        // parse points and create path data
+        const coordinates = points.trim().split(/\s+/).map(Number);
+        if (coordinates.length < 6 || coordinates.length % 2 !== 0) return;
+
+        let pathData = `m ${coordinates[0]} ${coordinates[1]}`;
+        for (let i = 2; i < coordinates.length; i += 2) {
+            const dx = coordinates[i] - coordinates[i - 2];
+            const dy = coordinates[i + 1] - coordinates[i - 1];
+            pathData += ` ${dx} ${dy}`;
+        }
+        pathData += "z";
+        pathDefs.push(pathData);
+    });
+
+    // create new path elements
+    const paths = pathDefs.map(d =>
+        <path key={uuidv4()} d={d} />
+    );
+
+    return (
+        <g
+            key={uuidv4()}
+            transform={g.getAttribute("transform") || ""}
+            className={g.classList.toString() + " hover:cursor-pointer"}
+            strokeLinejoin="round"
+            fill={color}
+            fillRule="nonzero"
+            stroke="#000000"
+            strokeWidth="1.0"
+            data-code={g.getAttribute("data-code") || ""}
+            onMouseOver={onMouseOver}
+            onMouseLeave={onMouseLeave}
+            onClick={onClick}
+        >
+            <title>{tooltip}</title>
+            {paths}
+        </g >
+    );
+
+}
+
+const generateReactG = (g: SVGElement, color: string, tooltip: string, onMouseOver: () => void, onMouseLeave: () => void, onClick: (e: React.MouseEvent<SVGGElement, MouseEvent>) => void) => {
+    // no polygons, create a new react element
+    // and keep the original children
+    const children = Array.from(g.childNodes).map((child) => {
+        if (child.nodeType === Node.ELEMENT_NODE) {
+            // create a jsx element for each child element
+            const element = child as Element;
+            // if it's a title element
+            if (element.tagName === 'title') {
+                // create a jsx title element
+                return <title key={uuidv4()}>{tooltip}</title>;
+                // if it's a path element
+            } else if (element.tagName === 'path') {
+                // create a jsx path element
+                const pathElement = element as SVGPathElement;
+                return <path
+                    key={uuidv4()}
+                    d={pathElement.getAttribute('d') || ""}
+                />;
+            }
+        }
+        // if it's another kind (which there isn't) return null
+        return null;
+    });
+    return (
+        <g
+            key={uuidv4()}
+            transform={g.getAttribute("transform") || ""}
+            className={g.classList.toString() + " hover:cursor-pointer"}
+            fill={color}
+            stroke={g.getAttribute("stroke") || "#000000"}
+            strokeWidth={g.getAttribute("stroke-width") || "1.0"}
+            onMouseOver={onMouseOver}
+            onMouseLeave={onMouseLeave}
+            data-code={g.getAttribute("data-code") || ""}
+            onClick={onClick}
+        >
+            {children}
+        </g>
+    );
 }
